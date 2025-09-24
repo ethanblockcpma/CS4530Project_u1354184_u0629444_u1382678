f4dc6c30ebc43a7f99159af1749e03e92f74b105
leoplute <leoplute@gmail.com>
2025-09-24 15:37:30 -0600

    Most of the ViewModel should be good. Definitely gonna need to update as we go but this should have the most important aspects.

---
+++ app/src/main/java/com/example/drawingapplication/DrawingViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.drawingapplication
+
+import androidx.compose.ui.geometry.Offset
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class DrawingViewModel : ViewModel() {
+
+    //Drawing state
+    private val penColor = MutableStateFlow("black")
+    val penColorReadOnly : StateFlow<String> = penColor
+
+    fun changePenColor(color : String){
+        penColor.value = color
+    }
+
+    private val penSize = MutableStateFlow(1)
+    val penSizeReadonly : StateFlow<Int> = penSize
+
+    fun changePenSize(newSize : Int){
+        penSize.value = newSize
+    }
+
+    private val penShape = MutableStateFlow("circle")
+    val penShapeReadOnly : StateFlow<String> = penShape
+
+    fun changePenShape(newShape : String){
+        penShape.value = newShape
+    }
+
+    // Drawing paths
+
+    // each inner list represents one complete stroke (pen down -> drag -> pen up)
+    private val strokes = MutableStateFlow<List<List<Offset>>>(emptyList())
+    val strokesReadOnly : StateFlow<List<List<Offset>>> = strokes
+
+    private val currentStroke = MutableStateFlow<List<Offset>>(emptyList())
+    val currentStrokReadOnly : StateFlow<List<Offset>> = currentStroke
+
+    fun startStokes(offset : Offset){
+        currentStroke.value = listOf(offset)
+        strokes.value = strokes.value + listOf(currentStroke.value)
+    }
+
+    fun addToStroke(offset : Offset){
+        currentStroke.value = currentStroke.value + offset
+        strokes.value = strokes.value.dropLast(1) + listOf(currentStroke.value)
+    }
+
+    fun endStroke(){
+        currentStroke.value = emptyList()
+    }
+
+}
