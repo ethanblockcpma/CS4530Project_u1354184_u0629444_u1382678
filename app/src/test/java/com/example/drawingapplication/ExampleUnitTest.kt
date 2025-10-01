3cce11a5a40ceda15d74afc9ca7cd2c6d31a49e8
leoplute <leoplute@gmail.com>
2025-10-01 12:07:56 -0600

    Tests added.

--- app/src/test/java/com/example/drawingapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/drawingapplication/ExampleUnitTest.kt
@@ -1,9 +1,11 @@
 package com.example.drawingapplication
 
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.junit.Test
 
 import org.junit.Assert.*
-
+import com.example.drawingapplication.DrawingViewModel
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +16,45 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun colorChangeCorrectly(){
+        val vm = DrawingViewModel()
+
+        assertEquals(Color.Black, vm.penColorReadOnly.value)
+
+        vm.changePenColor(Color.Blue)
+
+        assertEquals(Color.Blue, vm.penColorReadOnly.value)
+    }
+
+    @Test
+    fun changeSizeCorrectly(){
+        val vm = DrawingViewModel()
+        assertEquals(8f, vm.penSizeReadOnly.value)
+
+        vm.changePenSize(20f)
+        assertEquals(20f, vm.penSizeReadOnly.value)
+    }
+
+    @Test
+    fun changeShapeCorrectly(){
+        val vm = DrawingViewModel()
+        assertEquals("circle", vm.penShapeReadOnly.value)
+
+        vm.changePenShape("rectangle")
+        assertEquals("rectangle", vm.penShapeReadOnly.value)
+    }
+
+    @Test
+    fun toggleCorrectly(){
+        val vm = DrawingViewModel()
+        vm.changeOrTogglePenOptions(DrawingViewModel.PenOptions.COLOR)
+        assertEquals(DrawingViewModel.PenOptions.COLOR, vm.penOptionsShownReadOnly.value)
+
+        vm.changeOrTogglePenOptions(DrawingViewModel.PenOptions.COLOR)
+
+        assertEquals(DrawingViewModel.PenOptions.NONE, vm.penOptionsShownReadOnly.value)
+    }
+
 }
